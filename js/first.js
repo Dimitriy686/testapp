eab2efccc619afdf9592113c3625eb3edc018dcb
dmitriy <stratonov686@gmail.com>
2022-08-03 00:10:37 +0300

    footer

--- js/first.js
+++ js/first.js
@@ -1,5 +1,21 @@
-'use strict'
+/*'use strict'
 alert( 'Привет, брат!' );
-let message;
-message= 'HELLO';
-alert(message);
+let user= 'Dmitriy';
+let age= 40;
+let message ='HELLO';
+const myBirthday = '09.09.1981';
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+// ...когда нам нужно выбрать цвет
+let color = COLOR_ORANGE;
+// alert(color); // #FF7F00
+
+let name, admin;
+name  = "Dimitriy";
+admin = name;
+alert (admin);
+let name = prompt('Как тебя зовут?',"" );
+
+alert(`Меня зовут ${name} !`);*/
